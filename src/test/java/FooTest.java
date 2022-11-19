785cf86aa12614b7a89c75c8f933126aab5b4715
Julio Wong <julioo.wong@gmail.com>
2022-11-19 12:56:43 -0500

    commit

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -7,8 +7,8 @@ import com.utp.taller.demo.service.BarImpl;
 public class FooTest {
 	
 	@Test
-    public void validateSizeMap() {
+	public void validateSizeMap() {
 		Bar bar = new BarImpl();
 		assertEquals(4, bar.getResult().size());
-    }
+	}
 }
