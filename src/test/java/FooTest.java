c1ddb5edb74059c895393dc390f0e33c5a2c4b36
Julio Wong <julioo.wong@gmail.com>
2022-11-19 00:28:51 -0500

    commit

---
+++ src/test/java/FooTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import com.utp.taller.demo.service.Bar;
+import com.utp.taller.demo.service.BarImpl;
+
+public class FooTest {
+	
+	@Test
+    public void validateSizeMap() {
+		Bar bar = new BarImpl();
+		assertEquals(3, bar.getResult().size());
+    }
+}
